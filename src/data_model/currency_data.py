b01493594f80851d689f5913c1d61d7f55b2ab55
Maksymilian Sekula <maksymilian.sekula@hotmail.com>
2025-08-23 23:09:31 +0100

    Add rudimentary data model for currency codes

--- src/data_model/currency_data.py
+++ src/data_model/currency_data.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 
-# TODO: Define a currency code type with appropriate validation 
+class CurrencyCodes(BaseModel):
+    currencies: set[str]
 
 class ForexDataWithBase(BaseModel):
     base: str
